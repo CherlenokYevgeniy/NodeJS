c4483681fa5bbc55444fa4f0b035b5db54aa1443
Zheka <38732498+CherlenokYevgeniy@users.noreply.github.com>
2019-11-09 22:19:45 +0600

    create server in node.js / create local module
    use this local model in server

---
+++ app.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var mod = require('./modes.js');
+var server = http.createServer(function(request,response){
+ if(request.url =='/'){
+  response.write(mod.first("5"));
+  response.end();
+}
+else if(request.url =='/anotherex'){
+ response.write(mod.sec("7"));
+ response.end();
+}
+
+});
+server.listen(3000);
+console.log('node.js web server at 3000 is running..');
---
+++ modes.js
@@ -0,0 +1,10 @@
+function fir(number){
+ return "2+2 = " + number;
+}
+function sec(number){
+ return "3+4 = " + number;
+}
+module.exports = {
+ first:fir,
+ sec:sec
+}
